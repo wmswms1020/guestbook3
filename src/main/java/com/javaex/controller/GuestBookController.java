f3b428695b05791d2ee751cc6518aefd61eb8a20
sojunho <wmswms1020-@naver.com>
2022-06-14 10:18:05 +0900

    //

--- src/main/java/com/javaex/controller/GuestBookController.java
+++ src/main/java/com/javaex/controller/GuestBookController.java
@@ -1,13 +1,45 @@
 package com.javaex.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.javaex.dao.GuestbookDao;
+import com.javaex.vo.GuestbookVo;
 
 @Controller
 public class GuestBookController {
 	
+	@RequestMapping(value="/list", method = {RequestMethod.GET, RequestMethod.POST})
+	public String list(Model model) {
+		System.out.println("list");
+		
+		GuestbookDao guestbookDao = new GuestbookDao();
+		List<GuestbookVo> guestList = guestbookDao.getList();
+		System.out.println(guestList);		
+		
+		
+		return "WEB-INF/views/list.jsp";
+	}
 	
+	//등록하기
+	@RequestMapping(value="/add", method={RequestMethod.GET, RequestMethod.POST})
+	public String add(@ModelAttribute GuestbookVo vo) {
+		System.out.println("add");
+		
+		GuestbookDao guestbookDao = new GuestbookDao();
+		int count = guestbookDao.insert(vo);
+		
+		System.out.println(count);
+		
+		
+		return "redirect:list";
+		
+	}
 	
 	
 	
---
+++ webapp/WEB-INF/views/list.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	
+	<form action="./add" method="get">
+		<table>
+			<tr>
+				<td>이름</td>
+				<td><input type="text" name="name" value=""></td>
+				<td>비밀번호</td>
+				<td><input type="password" name="password" value=""></td>
+				
+			</tr>	
+			<tr>
+				<td><button type="submit">등록</button></td>
+			
+			
+			</tr>
+		
+		
+		
+		
+		</table>
+	</form>
+	
+
+</body>
+</html>
